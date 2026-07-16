a0faaac74f35c23a947d9c221ed19102d15d0094
Sujira Ploysumlee <ploysumlee_s@silpakorn.edu>
2026-07-16 05:10:16 +0000

    feweek02-edit

--- week02-lab/lab1-variables.js
+++
@@ -1,5 +0,0 @@
-const fullname = "สุจิรา พลอยสำลี";
-let age = 20;
-//fullname = "eiei";
-let output = `สวัสดี ฉันชื่อ${fullname} อายุ ${age} ปี และปีหน้าฉันจะอายุ ${age + 1} ปี.`;
-console.log(output);
